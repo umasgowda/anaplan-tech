e5f61576245d983c10803fcd896f55b1f7c2262b
umasgowda <umasgowda@gmail.com>
2021-12-09 09:21:49 +0000

    image tagging

--- bring-your-own-interview/docker-build.sh
+++ bring-your-own-interview/docker-build.sh
@@ -1,2 +1,2 @@
 #tag the image as umashivalingaiah2/dashboards
-docker build --build-arg JAR_FILE=target/bring-your-own-interview*.jar -t umashivalingaiah2/dashboards .
+docker build --build-arg JAR_FILE=target/bring-your-own-interview*.jar -t usg/dashboards .
