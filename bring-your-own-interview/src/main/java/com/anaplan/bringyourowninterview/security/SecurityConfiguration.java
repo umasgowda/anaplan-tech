204cc3211074738f94bb19c50e8b230a68fb7fa3
umasgowda <umasgowda@gmail.com>
2021-12-09 22:03:20 +0000

    added basic auth check for dashboard API's

--- bring-your-own-interview/src/main/java/com/anaplan/bringyourowninterview/security/SecurityConfiguration.java
+++ bring-your-own-interview/src/main/java/com/anaplan/bringyourowninterview/security/SecurityConfiguration.java
@@ -1,46 +1,46 @@
 package com.anaplan.bringyourowninterview.security;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * Added security(basic authentication) to restrict the /manage endpoints to an ADMIN role.
- * Automatically applies basic authentication to all exposed /manage endpoints.
+ * Added security(basic authentication) to restrict the /manage endpoints and some of the dashboards API's such as POST, DELETE, PUT
  */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Value("${security.user.name}")
-    private String username;
-
-    @Value("${security.user.password}")
-    private String password;
-
-    @Value("${management.security.roles}")
-    private String role;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser(username)
-                .password(encoder.encode(password))
-                .roles(role);
+                .withUser("user").password(encoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder().encode("password")).roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatcher(EndpointRequest.toAnyEndpoint())
-                .authorizeRequests((requests) -> requests.anyRequest()
-                        .hasRole(role));
-        http.httpBasic();
+        http.
+                csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**/dashboards").permitAll() //allows GET /dashboards requests for any user - no basic auth required
+                .anyRequest().authenticated() // All other API endpoints needs to be authenticated meaning they have to provide basic auth
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
